25eb741f2ab6766d73688846d55b91a4542873b9
ferrandlou <ferrandlou@9be03051-ea23-e3d6-ec44-74426a373fad>
2010-04-22 12:38:16 +0000

    Ajout des Exceptions

---
+++ JEE Project/src/java/qcm/exceptions/UnauthorizedActionException.java
@@ -0,0 +1,13 @@
+package qcm.exceptions;
+
+/**
+ *
+ * @author marya
+ */
+public class UnauthorizedActionException extends Exception{
+
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
+    
+}
