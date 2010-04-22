a4513c39f6013261e832a1493e97c39f9be9b366
ferrandlou <ferrandlou@9be03051-ea23-e3d6-ec44-74426a373fad>
2010-04-22 12:51:44 +0000

    Commit de l'indexAction

--- JEE Project/src/java/qcm/actions/IndexAction.java
+++ JEE Project/src/java/qcm/actions/IndexAction.java
@@ -1,53 +1,41 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package qcm.actions;
 
 import java.sql.SQLException;
+import qcm.exceptions.UnknownUserException;
 import qcm.models.User;
 import qcm.services.ActionHelper;
-import qcm.services.Helper;
 
 /**
  *
  * @author marya
  */
-public class IndexAction extends AbstractAction{
+public class IndexAction extends AbstractAction {
 
-    
-
-
-
-
-    public void execute() throws SQLException,Exception{
+    public void execute() throws SQLException, UnknownUserException {
 
         String page = "/scripts/login.jsp";
-        Helper helper = new Helper();
-       
-        if(!isUserAuthentificated()){
+
+        if (!isUserAuthentificated()) {
             String login = request.getParameter("login").toString();
             String password = request.getParameter("password").toString();
             if (login == null || login.trim().isEmpty()) {
-                throw new Exception("Veuillez renseigner votre login");
+                throw new UnknownUserException("Veuillez renseigner votre login");
             } else if (password == null || password.trim().isEmpty()) {
-                throw new Exception("Veuillez renseigner votre mot de passe");
-            }else{
-                User user = ActionHelper.checkUserByLoginAndPassWord(login,password);
-                if(user != null){
+                throw new UnknownUserException("Veuillez renseigner votre mot de passe");
+            } else {
+                User user = ActionHelper.checkUserByLoginAndPassWord(login, password);
+                if (user != null) {
                     request.getSession().setAttribute("user", user);
                     page = "/user/accueil.jsp";
-                }else{
-                    throw new Exception("Utilisateur inconnu : "+login);
+                } else {
+                    throw new UnknownUserException("Utilisateur inconnu : " + login);
                 }
             }
-        }else{
+        } else {
             page = "/user/accueil.jsp";
         }
         setView(page);
-       
-        
-    }
 
+
+    }
 }
