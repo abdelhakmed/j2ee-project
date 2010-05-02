3c92d86602301a124d50d42f90a3384d483ad366
maryaRabarison <maryaRabarison@9be03051-ea23-e3d6-ec44-74426a373fad>
2010-05-02 14:25:24 +0000

    Rajout user dans statistiques action

--- trunk/JEE Project/src/java/qcm/actions/mesQuestionnaires/MesQuestionnairesStatistiquesAction.java
+++ trunk/JEE Project/src/java/qcm/actions/mesQuestionnaires/MesQuestionnairesStatistiquesAction.java
@@ -1,9 +1,16 @@
 package qcm.actions.mesQuestionnaires;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import qcm.actions.EnseignantAction;
+import qcm.exceptions.UnauthorizedActionException;
 import qcm.models.QuestionnairePasse;
+import qcm.models.User;
+import qcm.persistences.QuestionnaireDAO;
 import qcm.persistences.QuestionnairePasseDAO;
+import qcm.persistences.UserDAO;
+import qcm.services.ActionHelper;
 
 /**
  *
@@ -14,8 +21,20 @@ public class MesQuestionnairesStatistiquesAction extends EnseignantAction {
     @Override
     public void execute() throws Exception {
         Integer idQuestionnaire = Integer.parseInt(request.getParameter("questionnaire"));
+        if(idQuestionnaire == null){
+            throw new UnauthorizedActionException("Merci de renseigner le questionnaire");
+        }
+        if(QuestionnaireDAO.getById(idQuestionnaire).getIdUser() != ActionHelper.getIdUser(request)){
+             throw new UnauthorizedActionException("Vous n'avez pas le droit de visualiser le statistique de ce questionnaire car vous n'êtes pas son créateur");
+        }
         List<QuestionnairePasse> usages = QuestionnairePasseDAO.findUsageById(idQuestionnaire);
+        Map<Integer, String> users = new HashMap<Integer, String>();
+        for(QuestionnairePasse qP : usages){
+            User u = UserDAO.getById(qP.getIdUser());
+            users.put(u.getIdUser(),u.getPrenom()+" "+u.getNom());
+        }
         request.setAttribute("usages", usages);
+        request.setAttribute("users", users);
         setView("/mesQuestionnaires/statistiques.jsp");
     }
 
